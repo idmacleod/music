d7cb4d3b4768bb3a4eed5eddbde822a7db0dc470
Alan <alanhood77@gmail.com>
2020-04-15 16:09:22 +0100

    Adding save() method to Artist class

--- models/artist.rb
+++ models/artist.rb
@@ -11,8 +11,12 @@ class Artist
     def initialize(options)
         @id = options['id'].to_i if options['id']
         @name = options['name']
-        
     end
-    
 
+    def save()
+        sql = "INSERT INTO artists (name) VALUES ($1) RETURNING id"
+        values = [@name]
+        @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    end
+       
 end
